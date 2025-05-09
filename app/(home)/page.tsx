20648fe09c1f9d82a74de845c0f625ee73162e76
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-09 23:43:00 +0900

    Add files via upload

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,10 @@
-import dynamic from "next/dynamic";
-
-// 클라이언트 컴포넌트인 SearchComponent를 동적으로 가져옵니다.
-const SearchComponent = dynamic(() => import("./search-component"), {
-  ssr: false, // 서버 사이드 렌더링을 비활성화합니다 (클라이언트 전용)
-});
+import { Suspense } from "react";
+import SearchComponent from "./search-component";
 
 export default function Page() {
-  return <SearchComponent />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchComponent />
+    </Suspense>
+  );
 }
