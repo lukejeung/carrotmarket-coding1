8fbfefcf6439ad961d2e1cdf9940b07ec5ec56c3
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-11 20:13:11 +0900

    Update route.ts

--- app/api/tweets/[id]/like/route.ts
+++ app/api/tweets/[id]/like/route.ts
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 import { likeTweet } from "@/lib/tweet";
 import type { NextRequest } from "next/server";
 
-export async function POST(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function POST(req: NextRequest, context) {
   const tweetId = context.params.id;
 
   if (!tweetId) {
