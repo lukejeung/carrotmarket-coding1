53b5378f4e3bdd2013ef841ae24aed648a749840
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-12 00:11:57 +0900

    Update page.tsx

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -30,7 +30,7 @@ export default async function UserProfilePage({
     notFound();
   }
 
-  const isOwner = session?.id === profile?.user_no;
+  const isOwner = session?.user.id === profile?.user_no;
 
   return (
     <div className="p-6">
