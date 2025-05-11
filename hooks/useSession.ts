fc1c34e33dd03b929b282603afa5115d2e3bcdb1
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-12 00:37:13 +0900

    Update useSession.ts

--- hooks/useSession.ts
+++ hooks/useSession.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { getSession } from "next-auth/react";
-import { DefaultUser } from "next-auth"; // 타입 가져오기
+import { Session } from "next-auth"; // 타입 임포트 필요
 
 export function useSessionUser() {
-  const [user, setUser] = useState<DefaultUser | null>(null);
+  const [user, setUser] = useState<Session["user"] | null>(null); // ✅ 여기를 수정
 
   useEffect(() => {
     getSession().then((session) => {
