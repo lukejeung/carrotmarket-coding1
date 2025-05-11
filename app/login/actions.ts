04a819a82ab1ff1fa4080476e306c0f8a5f6230c
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-11 22:29:25 +0900

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -94,7 +94,7 @@ export async function logIn(
       if (!session) {
   return { error: "세션을 가져올 수 없습니다. 다시 로그인해주세요." };
 }
-      session.id = user!.user_no;
+      session.user.id = user!.user_no;
       await session.save();
       redirect("/");
     } else {
