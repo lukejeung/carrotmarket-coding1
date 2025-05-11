9d5ee2ee34ab414313a44586467af65582e20fb0
lukejeung <lukejeung11@dongyang.ac.kr>
2025-05-11 22:11:29 +0900

    Update route.ts

--- app/api/users/[username]/edit/route.ts
+++ app/api/users/[username]/edit/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { updateUserProfile } from "@/lib/user";
+import { updateProfile } from "@/lib/user";
 
 export async function POST(req: NextRequest) {
   // URL에서 username 추출
